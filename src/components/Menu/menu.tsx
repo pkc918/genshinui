e68348b7a6878c9a6215835c20ceb56f7608d13a
XiaoPan <2489964425@qq.com>
2022-04-16 18:36:17 +0800

    使用 useContext 传递index

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {createContext, useState} from "react";
 import classNames from "classnames";
 
 // 组件的展示方向
 type MenuMode = "horizontal" | "vertical"
+type SelectCallback = (selectedIndex: number) => void;
 
+// MenuProps
 export interface MenuProps {
   defaultIndex?: number; // 默认选中的item
   className?: string; // 类名
@@ -12,15 +14,38 @@ export interface MenuProps {
   onSelect?: (selectedIndex: number) => void; // select 方法
 }
 
+// 使用 useContext 作为组件间的传值
+interface IMenuContext {
+  index: number;
+  onSelect?: SelectCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({index: 0});
+
 const Menu: React.FC<MenuProps> = (props) => {
-  const {className, mode, style, children, defaultIndex} = props;
+  const {className, mode, style, children, defaultIndex, onSelect} = props;
+  const [currentActive, setActive] = useState(defaultIndex);
   const classes = classNames("genshin-menu", className, {
     "menu-vertical": mode === "vertical"
   });
+  // onSelect 的回调函数
+  const handleClick = (index: number) => {
+    setActive(index);
+    if (onSelect) {
+      onSelect(index);
+    }
+  };
+  // Context 需要传递出去的参数
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick
+  };
 
   return (
     <ul className={classes} style={style}>
-      {children}
+      <MenuContext.Provider value={passedContext}>
+        {children}
+      </MenuContext.Provider>
     </ul>
   );
 };
@@ -28,6 +53,6 @@ const Menu: React.FC<MenuProps> = (props) => {
 Menu.defaultProps = {
   defaultIndex: 0,
   mode: "horizontal"
-}
+};
 
 export default Menu;
