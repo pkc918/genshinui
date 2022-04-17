728da7852a6f1f25e7fad08e172e30beafaaa496
XiaoPan <2489964425@qq.com>
2022-04-17 12:53:20 +0800

    修改测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,18 +1,27 @@
-import React from "react";
-import {cleanup, fireEvent, render, RenderResult} from "@testing-library/react";
-import Menu, {MenuProps} from "./menu";
-import MenuItem from "./menuItem";
-import SubMenu from "./subMenu";
+import React from 'react';
+import {cleanup, fireEvent, render, RenderResult, waitFor} from '@testing-library/react';
+
+import Menu, {MenuProps} from './menu';
+import MenuItem from './menuItem';
+import SubMenu from './subMenu'
+
+import Transition from '../Transition/transition';
+Transition.defaultProps = {
+  unmountOnExit: false,
+  appear: true,
+};
 
 const testProps: MenuProps = {
-  defaultIndex: "0",
+  defaultIndex: '0',
   onSelect: jest.fn(),
-  className: "test"
+  className: 'test'
 };
+
 const testVerProps: MenuProps = {
-  defaultIndex: "0",
-  mode: "vertical" // 垂直
+  defaultIndex: '0',
+  mode: 'vertical'
 };
+
 const generateMenu = (props: MenuProps) => {
   return (
     <Menu {...props}>
@@ -23,73 +32,77 @@ const generateMenu = (props: MenuProps) => {
         disabled
       </MenuItem>
       <MenuItem>
-        pkc
+        xyz
       </MenuItem>
-      <SubMenu title="hahaha">
-        <MenuItem>hahaha1</MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>
+          drop1
+        </MenuItem>
       </SubMenu>
     </Menu>
   );
 };
 const createStyleFile = () => {
   const cssFile: string = `
-    .genshin-submenu{
+    .genshin-submenu {
       display: none;
     }
-    .genshin-submenu.menu-opend{
-      display: block;
+    .genshin-submenu.menu-opened {
+      display:block;
     }
-  `;
-  const style = document.createElement("style");
-  style.innerHTML = cssFile;
-  return style;
-};
-let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
+  `
+  const style = document.createElement('style')
+  style.innerHTML = cssFile
+  return style
+}
 
-describe("test Menu and MenuItem component", () => {
+let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
+describe('test Menu and MenuItem component', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
-    wrapper.container.append(createStyleFile());
-    menuElement = wrapper.getByTestId("test-menu");
-    activeElement = wrapper.getByText("active");
-    disabledElement = wrapper.getByText("disabled");
+    wrapper.container.append(createStyleFile())
+    menuElement = wrapper.getByTestId('test-menu');
+    activeElement = wrapper.getByText('active');
+    disabledElement = wrapper.getByText('disabled');
   });
-  it("should render correct Menu and MenuItem based on default props", function () {
+  it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument();
-    expect(menuElement).toHaveClass("genshin-menu test");
-    expect(menuElement.querySelectorAll(":scope > li").length).toEqual(4);
-    expect(activeElement).toHaveClass("menu-item is-active");
-    expect(disabledElement).toHaveClass("menu-item is-disabled");
+    expect(menuElement).toHaveClass('genshin-menu test');
+    // 只选择第一级的
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
+    expect(activeElement).toHaveClass('menu-item is-active');
+    expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
-  it("click items should change active and call the right callback", function () {
-    const thirdItem = wrapper.getByText("pkc");
+  it('click items should change active and call the right callback', () => {
+    const thirdItem = wrapper.getByText('xyz');
     fireEvent.click(thirdItem);
-    expect(thirdItem).toHaveClass("is-active");
-    expect(activeElement).not.toHaveClass("is-active");
+    expect(thirdItem).toHaveClass('is-active');
+    expect(activeElement).not.toHaveClass('is-active');
     expect(testProps.onSelect).toHaveBeenCalledWith("2");
     fireEvent.click(disabledElement);
-    expect(disabledElement).not.toHaveClass("is-active");
+    expect(disabledElement).not.toHaveClass('is-active');
     expect(testProps.onSelect).not.toHaveBeenCalledWith("1");
   });
-  it("should render vertical mode when mode is set to vertical", function () {
+  it('should render vertical mode when mode is set to vertical', () => {
     cleanup();
     const wrapper = render(generateMenu(testVerProps));
-    const menuElement = wrapper.getByTestId("test-menu");
-    expect(menuElement).toHaveClass("menu-vertical");
+    const menuElement = wrapper.getByTestId('test-menu');
+    expect(menuElement).toHaveClass('menu-vertical');
   });
-  it("should show dropdown items when hover on subMenu", async () => {
-    expect(wrapper.queryByText("hahaha1")).not.toBeVisible();
-    const dropdownElement = wrapper.getByText("hahaha");
-    fireEvent.mouseEnter(dropdownElement);
-    // 这里有所不同，直接使用了 await
-    await (() => {
-      expect(wrapper.queryByText("hahaha1")).toBeVisible();
-    });
-    fireEvent.click(wrapper.getByText("hahaha1"));
-    expect(testProps.onSelect).toHaveBeenCalledWith("3-0");
-    fireEvent.mouseLeave(dropdownElement);
-    await (() => {
-      expect(wrapper.queryByText("hahaha1")).not.toBeVisible();
-    });
-  });
+  it('should show dropdown items when hover on subMenu', async ()=>{
+    // 和getByText的区别，返回htmlElement或者是null，这里可能不存在
+    expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    const dropdownElement = wrapper.getByText('dropdown')
+    fireEvent.mouseEnter(dropdownElement)
+
+    await waitFor(() => {
+      expect(wrapper.queryByText('drop1')).toBeVisible()
+    })
+    fireEvent.click(wrapper.getByText('drop1'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    fireEvent.mouseLeave(dropdownElement)
+    await waitFor(() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible();
+    })
+  })
 });
