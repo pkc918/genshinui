ba204187511f79335a173399c12b422b9ab11acb
XiaoPan <2489964425@qq.com>
2022-04-23 11:39:28 +0800

    将 input 组件暴露出去

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,28 @@
-// import {library} from "@fortawesome/fontawesome-svg-core";
-// import {fas} from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+
+export {default as Button} from "./components/Button";
+export {default as Menu} from "./components/Menu";
+export {default as Icon} from "./components/Icon";
+export {default as Transition} from "./components/Transition";
+export {default as Input} from "./components/Input";
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles/index.scss';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 //
-// library.add(fas);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 //
-// export {default as Button} from "./components/Button";
-// export {default as Menu} from "./components/Menu";
-// export {default as Icon} from "./components/Icon";
-// export {default as Transition} from "./components/Transition";
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
